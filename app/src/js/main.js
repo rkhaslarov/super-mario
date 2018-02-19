e14ca522feb108abcae6bf6f1105aa6338db2e0f
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2018-02-19 21:57:48 +0300

    add mario

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,25 +1,87 @@
 import { loadImage } from './helpers/utils';
 import { drawBackground, loadLevel } from './helpers/level';
 import SpriteSheet from './components/SpriteSheet';
+import Composer from './components/Composer';
 
 const canvas = document.querySelector('#main');
 const context = canvas.getContext('2d');
 
-loadImage('/images/tiles.png')
-    .then(image => {
+function loadBackgroundSprites() {
+    return loadImage('/images/tiles.png')
+        .then(image => {
 
-        const sprites = new SpriteSheet(image, 16, 16);
+            const sprites = new SpriteSheet(image, 16, 16);
 
-        sprites.define('ground', 0, 0)
-        sprites.define('sky', 3, 23)
+            sprites.defineTile('ground', 0, 0);
+            sprites.defineTile('sky', 3, 23);
 
-        loadLevel('1-1')
-            .then(level => {
-                level.backgrounds.forEach(bg => {
-                    drawBackground(bg, context, sprites);
-                });
-            })
-    })
-    .catch((e) => {
-        console.error(e)
+            return sprites;
+        })
+}
+
+function loadCharacterSprite() {
+    return loadImage('/images/characters.gif')
+        .then(image => {
+
+            const sprites = new SpriteSheet(image, 16, 16);
+
+            sprites.define('idle', 276, 44, 16, 16)
+
+            return sprites;
+        })
+}
+
+function createBackgroundLayer(backgrounds, sprites) {
+    const buffer = document.createElement('canvas');
+    buffer.width = 256;
+    buffer.height = 240;
+
+    backgrounds.forEach(background => {
+        drawBackground(background, buffer.getContext('2d'), sprites);
     });
+
+    return function drawBackgroundLayer(context) {
+        context.drawImage(buffer, 0, 0);
+    };
+}
+
+function createSpriteLayer(sprite, pos) {
+    return function drawSpriteLayer(context) {
+        sprite.draw('idle', context, pos.x, pos.y);
+    };
+}
+
+Promise.all([
+    loadBackgroundSprites(),
+    loadCharacterSprite(),
+    loadLevel('1-1')
+])
+.then(([background, mario, level]) => {
+
+    const composer = new Composer();
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, background);
+
+    composer.layers.push(backgroundLayer);
+
+    const pos = {
+        x: 64,
+        y: 64
+    }
+    const marioLayer = createSpriteLayer(mario, pos);
+
+    composer.layers.push(marioLayer);
+
+    function update() {
+        composer.draw(context);
+        mario.draw('idle', context, pos.x, pos.y);
+        pos.x += 2;
+        pos.y += 2;
+
+        requestAnimationFrame(update);
+    }
+
+    update();
+})
+.catch((e) => {
+    console.error(e)
+});
